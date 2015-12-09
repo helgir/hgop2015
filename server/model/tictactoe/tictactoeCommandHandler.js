33c57adc01f917359d37211659853be94ac91502
helgir <helgir87@gmail.com>
2015-12-09 15:55:46 +0000

    made test when make a move

--- server/model/tictactoe/tictactoeCommandHandler.js
+++ server/model/tictactoe/tictactoeCommandHandler.js
@@ -1,7 +1,50 @@
+var _ = require('lodash');
 module.exports = function tictactoeCommandHandler(events) {
   var gameCreatedEvent = events[0];
 
+  var gameState = {
+    gameCreatedEvent : events[0],
+    board: [['','',''],['','',''],['','','']]
+  };
+
+  var eventHandlers={
+    'MoveMade': function(event){
+      gameState.board[event.x][event.y] = event.side;
+    }
+  };
+
+  _.each(events, function(event){
+    var eventHandler = eventHandlers[event.event];
+    if(eventHandler) eventHandler(event);
+  });
+
+
   var handlers = {
+    "MakeMove": function(cmd){
+      if(gameState.board[cmd.x][cmd.y]!==''){
+        return [{
+          id: cmd.id,
+          event: "IllegalMove",
+          userName: cmd.userName,
+          name:gameState.gameCreatedEvent.name,
+          x:cmd.x,
+          y:cmd.y,
+          side:cmd.side,
+          timeStamp: cmd.timeStamp
+        }];
+      }
+      return [{
+        id: cmd.id,
+        event: "MoveMade",
+        userName: cmd.userName,
+        name:gameState.gameCreatedEvent.name,
+        x:cmd.x,
+        y:cmd.y,
+        side:cmd.side,
+        timeStamp: cmd.timeStamp
+      }];
+    },
+
     "CreateGame": function (cmd) {
       {
         return [{
