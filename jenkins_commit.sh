02071737618c03b74dc881bb3889911984ad8b18
helgir <helgir87@gmail.com>
2015-12-15 16:42:35 +0000

    Fixed jenkins_commit

--- jenkins_commit.sh
+++ jenkins_commit.sh
@@ -1,28 +1,31 @@
+#!/bin/bash
 echo 'Starting jenkins commit'
 
-export PATH=/usr/local/bin:/path/to/node:/path/to/node_bin:/path/to/phantomjs:/path/to/jscoverage:$PATH;
+export PATH=/usr/local/bin:$PATH
 export DISPLAY=:0
 
+echo 'Starting npm install'
 npm install
+echo 'Starting bower install'
 bower install
-
-./dockerbuild.sh
+echo 'Starting dockerbuild'
+./bin/Docker/dockerbuild.sh
 exitcode=$?
 if [ $exitcode != 0 ]; then
-	exit $exitcode
+exit $exitcode
 fi
 
 echo 'Logging in to docker'
 docker login --username=helgir --password=$DOCKER_PASS --email=$DOCKER_EMAIL &&
 exitcode=$?
-	if [ $exitcode != 0 ]; then
-		echo Docker login failed.
-		exit $exitcode
-	fi
+if [ $exitcode != 0 ]; then
+echo Docker login failed.
+exit $exitcode
+fi
 echo 'Pushing to docker'
 docker push helgir/tictactoe
 exitcode=$?
-	if [ $exitcode != 0 ]; then
-		echo Docker push failed.
-		exit $exitcode
-	fi
+if [ $exitcode != 0 ]; then
+echo Docker push failed.
+exit $exitcode
+fi
