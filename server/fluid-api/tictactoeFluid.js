5aa081e8dffc0010f9e07910d39b30847e8d3a71
helgir <helgir87@gmail.com>
2015-12-17 14:02:38 +0000

    Made crate game with loop work in acceptance test

--- server/fluid-api/tictactoeFluid.js
+++ server/fluid-api/tictactoeFluid.js
@@ -1,5 +1,6 @@
 'use strict';
 
+var loop = require('lodash');
 var should = require('should');
 var request = require('supertest');
 var acceptanceUrl = process.env.ACCEPTANCE_URL;
@@ -8,47 +9,63 @@ function given(userApi) {
   var _expectedEvents=[{
     "id": "1234",
     "gameId": userApi._command.gameId,
+    "name": userApi._command.name,
     "event": "EventName",
-    "userName": userApi._command.userName,
-    "name": userApi._command.gameId,
+    "userName": userApi._command.userName
   }];
+
   var _currentEvent = 0;
+
+  var _users = [
+    userApi
+  ];
+
   var expectApi = {
     and: function (userCommand) {
       userCommand.name = userApi.name;
       userCommand.gameId = userApi.gameId;
       userCommand.otherUserName = userApi.userName;
+      _users.push(userCommand);
       return expectApi;
     },
     withName: function (gameName) {
       _expectedEvents[_currentEvent].name = gameName;
       return expectApi;
     },
+    byUser: function (userName) {
+      _expectedEvents[_currentEvent].userName = userName;
+    },
     expect: function (eventName) {
       _expectedEvents[_currentEvent].event = eventName;
       return expectApi;
     },
+
     isOk: function (done) {
       var req = request(acceptanceUrl);
-      req
-        .post('/api/createGame')
-        .type('json')
-        .send(userApi._command)
+      loop.each(_users, function(userComm) {
+
+        req
+          .post(userComm._command.destination)
+          .type('json')
+          .send(userComm._command)
+          .end(function (err, res) {
+            if (err) return done(err);
+          });
+      });
+      request(acceptanceUrl)
+        .get('/api/gameHistory/' + userApi._command.gameId)
+        .expect(200)
+        .expect('Content-Type', /json/)
         .end(function (err, res) {
+
           if (err) return done(err);
-          request(acceptanceUrl)
-            .get('/api/gameHistory/' + userApi._command.gameId)
-            .expect(200)
-            .expect('Content-Type', /json/)
-            .end(function (err, res) {
-              if (err) return done(err);
-              res.body.should.be.instanceof(Array);
-              should(res.body).eql(
-                _expectedEvents);
-              done();
-            });
+          res.body.should.be.instanceof(Array);
+          should(res.body[res.body.length-1]).eql(
+            _expectedEvents[_expectedEvents.length-1]);
+
+          done();
         });
-      return expectApi;
+    return expectApi;
     },
   };
 
@@ -58,17 +75,22 @@ function given(userApi) {
 function user(userName) {
   var userApi = {
     _command: {
-      gameId: undefined,
       id: "1234",
       userName: userName,
       command: undefined,
       destination: undefined,
-      name: undefined
+      //name: undefined
     },
     createsGame: function (gameId) {
       userApi._command.gameId = gameId;
       userApi._command.command = "CreateGame";
-      userApi._command.destination = '/api/createsGame';
+      userApi._command.destination = '/api/createGame';
+      return userApi;
+    },
+    joinsGame: function (gameId) {
+      userApi._command.gameId = gameId;
+      userApi._command.command = "JoinGame";
+      userApi._command.destination = '/api/joinGame';
       return userApi;
     },
     named: function(gamename) {
